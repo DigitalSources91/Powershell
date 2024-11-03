de5933c2d65fb6570eeff87b6838b7bee9285c74
DigitalSources91 <logdigitales@gmail.com>
2024-11-03 17:17:45 -0500

    Add files via upload
    Comandos en powershell para ejecutar.

---
+++ Get-FileSizes.ps1
@@ -0,0 +1,7 @@
+﻿$Folder = split-path -parent $MyInvocation.MyCommand.Definition;
+Get-ChildItem -path $Folder -ErrorAction silentlycontinue -Recurse |
+Sort-Object -Property length -Descending |
+Format-Table -autosize -wrap -property `
+    @{Label=”Last access”;Expression={($_.lastwritetime).ToshortDateString()}},
+    @{label=”size in megabytes”;Expression={“{0:N2}” -f ($_.Length / 1MB)}},
+    fullname
---
+++ Imagen-disco.ps1
@@ -0,0 +1,10 @@
+﻿$DriveLetter = "C" # Cambia la letra de la unidad según la que desees crear la imagen de disco
+$ImagePath = "C:\Users\Usuario\Documents\ImageBackup.wim" # Cambia la ruta de la imagen de disco según la que desees utilizar
+
+New-Item -ItemType Directory -Force -Path $ImagePath.Substring(0, $ImagePath.LastIndexOf("\"))
+
+New-Item -ItemType File -Path $ImagePath -Force
+
+$Wimcmd = "New-WindowsImage -CapturePath $DriveLetter\ -ImagePath $ImagePath -Name 'Windows 10 Image Backup' -Description 'Image Backup created on $(Get-Date)' -Verify -CompressionType Maximum"
+
+Invoke-Expression $Wimcmd
